c93038b60bf75977dfc5a7b094a033c6e8ee25c8
Rohit Mohadikar <rohitmohadikar897@gmail.com>
2026-03-15 15:33:38 +0530

    Update SuccessPage.jsx

--- src/Pages/SuccessPage.jsx
+++ src/Pages/SuccessPage.jsx
@@ -3,24 +3,35 @@ import { useNavigate } from "react-router-dom";
 import styles from "./SuccessPage.module.css";
 
 function SuccessPage() {
+
   const navigate = useNavigate();
 
   useEffect(() => {
+
     const timer = setTimeout(() => {
       navigate("/");
     }, 3000);
 
     return () => clearTimeout(timer);
+
   }, []);
 
   return (
+
     <div className={styles.container}>
+
       <div className={styles.card}>
+
         <div className={styles.tick}>✓</div>
+
         <h2>Order Placed Successfully!</h2>
+
         <p>You will be redirected to home page...</p>
+
       </div>
+
     </div>
+
   );
 }
 
